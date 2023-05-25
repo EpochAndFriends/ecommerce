2b5e1e83f40ccc98c768f953801e72e819963e67
MaxiBrocato <maximilianobrocato@gmail.com>
2023-05-25 19:25:32 -0300

    pagina fuego comienzo

---
+++ planeta1.js
@@ -0,0 +1,39 @@
+function loadDescriptions() {
+    fetch('planeta1.json')
+      .then(response => response.json())
+      .then(descriptions => {
+        const selector1 = document.querySelector('.selector1');
+        const selector2 = document.querySelector('.selector2');
+        const selector3 = document.querySelector('.selector3');
+  
+        selector1.addEventListener('click', () => showOverlay('fuego-cueva.png', descriptions['fuego-cueva.png']));
+        selector2.addEventListener('click', () => showOverlay('fuego-playa.png', descriptions['fuego-playa.png']));
+        selector3.addEventListener('click', () => showOverlay('fuego-montaña.png', descriptions['fuego-montaña.png']));
+      })
+      .catch(error => console.log(error));
+  }
+  
+  function showOverlay(imageSrc, description) {
+    const overlay = document.getElementById('overlay');
+    const overlayImage = document.getElementById('overlay-image');
+    const overlayDescription = document.getElementById('overlay-description');
+  
+    overlayImage.src = imageSrc;
+    overlayDescription.textContent = description;
+  
+    overlay.style.display = 'flex';
+  }
+  
+  function hideOverlay() {
+    const overlay = document.getElementById('overlay');
+    overlay.style.display = 'none';
+  }
+  
+  function visitPage() {
+    // Redirigir al usuario a otra página
+    window.location.href = 'otra_pagina.html';
+  }
+  
+  // Llama a la función para cargar las descripciones
+  loadDescriptions();
+  
